638b08e15121ff0987bb241ae82b915882937bef
rudra33patel-png <rudra33patel@gmail.com>
2026-09-22 10:38:10 +0530

    page 1

--- app/src/main/java/com/mad_project_final/MainActivity.java
+++ app/src/main/java/com/mad_project_final/MainActivity.java
@@ -1,6 +1,10 @@
 package com.mad_project_final;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText e1, e2;
+    Button b1;
+    TextView t1, t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        e1 = findViewById(R.id.editTextText);
+        e2 = findViewById(R.id.editTextText2);
+        b1 = findViewById(R.id.button);
+        t1 = findViewById(R.id.textView);
+        t2 = findViewById(R.id.textView2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                if (s1.equals("admin") && s2.equals("1234")) {
+                    t1.setText("Login Successful");
+                } else {
+                    t1.setText("Invalid Login");
+                }
+            }
         });
     }
 }
