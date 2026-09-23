385e63eb4b2b68aec7a204c33e78a972b508608e
rudra33patel-png <rudra33patel@gmail.com>
2026-09-23 14:04:31 +0530

    page 2

--- app/src/main/java/com/mad_project_final/MainActivity2.java
+++ app/src/main/java/com/mad_project_final/MainActivity2.java
@@ -1,6 +1,5 @@
 package com.mad_project_final;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,46 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    EditText e1, e2;
+public class MainActivity2 extends AppCompatActivity {
+    EditText e1;
     Button b1;
-    TextView t1, t2;
+    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activitymain_2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        e1 = findViewById(R.id.editTextText);
-        e2 = findViewById(R.id.editTextText2);
-        b1 = findViewById(R.id.button);
-        t1 = findViewById(R.id.textView);
-        t2 = findViewById(R.id.textView2);
+        e1 = findViewById(R.id.editTextText6);
+        b1 = findViewById(R.id.button3);
+        t1 = findViewById(R.id.textView5);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s1 = e1.getText().toString();
-                String s2 = e2.getText().toString();
-                if (s1.equals("admin") && s2.equals("1234")) {
-                    t1.setText("Login Successful");
+                if (s1.equals("")) {
+                    t1.setText("Please enter email");
                 } else {
-                    t1.setText("Invalid Login");
+                    t1.setText("Password reset link sent");
                 }
-            }
-        });
-
-        t1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(i);
             }
         });
     }
